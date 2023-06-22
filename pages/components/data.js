d9bb505127f8bb161f43ba6eb3e4c0ffb5bd4117
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-06-21 21:40:49 -0300

    adds photos and new pages

--- pages/components/data.js
+++ pages/components/data.js
@@ -1,9 +1,317 @@
-export const curriculumData = {
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import lattes from "./assets/img/t/Lattes.png";
+
+import html from "./assets/img/t/html.png";
+import css from "./assets/img/t/css.png";
+import js from "./assets/img/t/js.png";
+import next from "./assets/img/t/next2.png";
+import react from "./assets/img/t/React2.png";
+import node from "./assets/img/t/node.png";
+import ts from "./assets/img/t/ts.png";
+import sc from "./assets/img/t/styled.png";
+import docker from "./assets/img/t/docker.png";
+import actions from "./assets/img/t/actions.png";
+import motion from "./assets/img/t/framer-motion.png";
+import mui from "./assets/img/t/mui.png"
+import rest from "./assets/img/t/rest.png"
+import velo from "./assets/img/t/velo.png"
+import mongodb from "./assets/img/t/mongodb.svg"
+
+import ormaLogo from "./assets/img/t/orma logo.svg";
+import gssLogo from "./assets/img/t/gss.svg";
+
+import etec from "./assets/img/t/etec.png";
+import unimonte from "./assets/img/t/unimonte.png";
+import unifesp from "./assets/img/t/unifesp.png";
+
+
+
+// export const enUS = {
+//   personal: {
+//     name: "Kevin Pereira Marangoni",
+//     position: "Frontend Web Developer",
+//     level: "Mid"
+//         salary: "R$ 3500 to R$6000"
+//         links: {
+//       github: "https://github.com/kevinmarangoni",
+//       linkedin: "https://www.linkedin.com/in/kevin-marangoni-1a3a17127/",
+//       lattes: "http://lattes.cnpq.br/5620060448568039",
+//       instagram: "https://www.instagram.com/kevinmarangoni/",
+//     }
+//         info: {
+//       about: "I have a Bachelor of Science in Biomedicine and a Master in Health Science’s, always fascinated by biology and in deep love with computers and programming."
+//             looking: "Opportunities to collaborate with projects and companies that I believe are concerned with the growth of society, that see people as people and respect individual differences and capabilities."
+//             want: "Collaborate with my knowledge of Fullstack Tools, Frameworks and many other tools to attend the company’s needs. I would like to work with a multidisciplinary teams, so I can always learn from different point of view of every coworker."
+//     }
+//   }
+//     work: [
+//     {
+//       logo: ormaLogo,
+//       companyName: "Orma Carbon",
+//       howLong: "since feb. 2022 to Oct. 2022",
+//       description: "Technological Solutions for Sustainability",
+//       position: "Frontend Web Developer Jr.",
+//       act: "Develops ReactJs/NextJs applications for the company",
+//       website: "https://www.orma.eco",
+//       isCurrent: false,
+//     },
+//     {
+//       logo: gssLogo,
+//       companyName: "GSS Carbono e Sustentabilidade",
+//       howLong: "since Oct. of 2022 to Apr. 2023",
+//       description: "Technological Solutions for Sustainability",
+//       position: "Fullstack Web Developer Jr.",
+//       act: "Develops ReactJs/NextJs and Node.Js applications for the company",
+//       website: "https://www.gss.eco",
+//       isCurrent: false,
+//     },
+//   ],
+//   education: [
+//     {
+//       instName: "Etec Dra. Ruth Cardoso",
+//       inDate: "01/2011",
+//       outDate: "12/2013",
+//       type: "High School",
+//       title: "Graduated",
+//       logo: etec,
+//     },
+//     {
+//       instName: "Etec Dra. Ruth Cardoso",
+//       inDate: "06/2012",
+//       outDate: "12/2013",
+//       type: "Technical School",
+//       title: "Computer Technician",
+//       logo: etec,
+//     },
+//     {
+//       instName: "São Judas University",
+//       inDate: "01/2015",
+//       outDate: "12/2018",
+//       type: "Biomedicine College",
+//       title: "Bachelor of Science",
+//       logo: unimonte,
+//     },
+//     {
+//       instName: "São Paulo Federal University",
+//       inDate: "01/2019",
+//       outDate: "09/2021",
+//       type: "Postgraduation College",
+//       title: "Master in Health Science’s",
+//       logo: unifesp,
+//     },
+//   ],
+//   stacks: [
+//     { logo: html, name: "HTML5", skill: 4 },
+//     { logo: css, name: "CSS3", skill: 4 },
+//     { logo: js, name: "Javascript", skill: 4 },
+//     { logo: ts, name: "Typescript", skill: 3 },
+//     { logo: next, name: "NextJS", skill: 4 },
+//     { logo: react, name: "ReactJS", skill: 4 },
+//     { logo: node, name: "NodeJS", skill: 3 },
+//     { logo: mongodb, name: "MongoDB", skill: 3 },
+//     { logo: sc, name: "Styled Components", skill: 3 },
+//     { logo: docker, name: "Docker", skill: 3 },
+//     { logo: actions, name: "Actions", skill: 3 },
+//     { logo: motion, name: "Framer Motion", skill: 3 },
+//     { logo: mui, name: "Material UI", skill: 3 },
+//     { logo: rest, name: "REST API", skill: 4 },
+//     { logo: velo, name: "Wix Velo", skill: 4 },
+//   ];
+//   portfolio: [
+//     {
+//       project: "Movimento Repenso",
+//       description: "A platform that allows small businesses to conduct their annual carbon emissions inventory can be developed based on internationally recognized methodologies and guidelines, such as the Greenhouse Gas Protocol. The platform can provide a structured questionnaire to collect information about the company's activities, such as energy consumption, transportation, waste, and other relevant sources."
+//             link: "https://www.repenso.eco",
+//       stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "NodeJS", "Styled Components", "REST API", "MySQL"]
+//             imgs: [],
+//     },
+//     {
+//       project: "Smartie Carbon",
+//       description: ""
+//             link: "",
+//       stacks: []
+//             imgs: [],
+//     },
+//     {
+//       project: "",
+//       description: ""
+//             link: "",
+//       stacks: []
+//             imgs: [],
+//     },
+//     {
+//       project: "",
+//       description: ""
+//             link: "",
+//       stacks: []
+//             imgs: [],
+//     },
+//   ]
+
+// }
+
+export const ptBR = {
     personal: {
+      name: "Kevin Pereira Marangoni",
+      position: "Desenvolvedor Web Fullstack",
+      level: "Pleno",
+      salary: "R$ 3500 a R$ 6000",
+      links: {
+        github: "https://github.com/kevinmarangoni",
+        linkedin: "https://www.linkedin.com/in/kevin-marangoni-1a3a17127/",
+        lattes: "http://lattes.cnpq.br/5620060448568039",
+        instagram: "https://www.instagram.com/kevinmarangoni/"
+      },
+      info: {
+        about: "Tenho Bacharelado em Biomedicina e Mestrado em Ciências da Saúde, sempre fascinado pela biologia e apaixonado por computadores e programação.",
+        looking: "Oportunidades para colaborar em projetos e empresas que acredito estarem preocupados com o crescimento da sociedade, que vejam as pessoas como pessoas e respeitem as diferenças e habilidades individuais.",
+        want: "Colaborar com meu conhecimento de Ferramentas Fullstack, Frameworks e muitas outras ferramentas para atender às necessidades da empresa. Gostaria de trabalhar com equipes multidisciplinares para poder aprender sempre com diferentes pontos de vista de cada colega de trabalho."
+      }
+    },
+    work: [
+      {
+        logo: ormaLogo,
+        companyName: "Orma Carbon",
+        howLong: "de fev. 2022 a out. 2022",
+        description: "Soluções Tecnológicas para Sustentabilidade",
+        position: "Desenvolvedor Web Frontend Jr.",
+        act: "Desenvolve aplicações ReactJs/NextJs para a empresa",
+        website: "https://www.orma.eco",
+        isCurrent: false
+      },
+      {
+        logo: gssLogo,
+        companyName: "GSS Carbono e Sustentabilidade",
+        howLong: "de out. 2022 a abr. 2023",
+        description: "Soluções Tecnológicas para Sustentabilidade",
+        position: "Desenvolvedor Web Fullstack Jr.",
+        act: "Desenvolve aplicações ReactJs/NextJs e Node.Js para a empresa",
+        website: "https://www.gss.eco",
+        isCurrent: false
+      }
+    ],
+    education: [
+      {
+        instName: "Etec Dra. Ruth Cardoso",
+        inDate: "01/2011",
+        outDate: "12/2013",
+        type: "Ensino Médio",
+        title: "Concluído",
+        logo: etec,
+      },
+      {
+        instName: "Etec Dra. Ruth Cardoso",
+        inDate: "06/2012",
+        outDate: "12/2013",
+        type: "Ensino Técnico",
+        title: "Técnico em Informática",
+        logo: etec
+      },
+      {
+        instName: "Universidade São Judas",
+        inDate: "01/2015",
+        outDate: "12/2018",
+        type: "Faculdade de Biomedicina",
+        title: "Bacharelado em Ciências Biomédicas",
+        logo: unimonte
+      },
+      {
+        instName: "Universidade Federal de São Paulo",
+        inDate: "01/2019",
+        outDate: "09/2021",
+        type: "Pós-Graduação",
+        title: "Mestrado em Ciências da Saúde",
+        logo: unifesp
+      }
+    ],
+    stacks: [
+      { logo: html, name: "HTML5", skill: 4 },
+      { logo: css, name: "CSS3", skill: 3 },
+      { logo: js, name: "Javascript", skill: 4 },
+      { logo: ts, name: "Typescript", skill: 3 },
+      { logo: next, name: "NextJS", skill: 4 },
+      { logo: react, name: "ReactJS", skill: 4 },
+      { logo: node, name: "NodeJS", skill: 3 },
+      { logo: mongodb, name: "MongoDB", skill: 3 },
+      { logo: sc, name: "Styled Components", skill: 3 },
+      { logo: docker, name: "Docker", skill: 3 },
+      { logo: actions, name: "Actions", skill: 3 },
+      { logo: motion, name: "Framer Motion", skill: 3 },
+      { logo: mui, name: "Material UI", skill: 3 },
+      { logo: rest, name: "REST API", skill: 4 },
+      { logo: velo, name: "Wix Velo", skill: 3 },
+      { logo: "", name: "MySQL", skill: 3 },
+    ], 
+    softskills: [
+      {name: "Empatia",skill: "",},
+      {name: "Comunicação efetiva",skill: "",},
+      {name: "Colaboração",skill: "",},
+      {name: "Pensamento crítico",skill: "",},
+      {name: "Resolução de problemas",skill: "",},
+      {name: "Adaptabilidade",skill: "",},
+      {name: "Autodisciplina",skill: "",},
 
-    }
-    education:{
+    ],
+    portfolio: [
+      {
+        project: "Movimento Repenso",
+        description: "Uma plataforma que permite que pequenas empresas realizem seu inventário anual de emissões de carbono com base em metodologias e diretrizes internacionalmente reconhecidas, como o Protocolo de Gases de Efeito Estufa. A plataforma pode fornecer um questionário estruturado para coletar informações sobre as atividades da empresa, como consumo de energia, transporte, resíduos e outras fontes relevantes. A plataforma conta com sistema de login, sistema de pagamentos e dashboard de usuários e admin, assim como CMS para gerenciamento de conteúdos do blog.",
+        link: "https://www.repenso.eco",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "GoLang", "Styled Components", "REST API", "MySQL"],
+        imgs: []
+      },
+      {
+        project: "Smartie Carbon",
+        description: "Plataforma construida pela Orma Carbon para Solví a fim de promover a venda e compra de créditos de carbono, possui uma calculadora de emissões de automóveis e outra para emissões do dia-a-dia. A plataforma conta com sistema de login",
+        link: "https://www.smartiecarbon.com.br",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "NodeJS", "Styled Components", "REST API", "MySQL"],
+        imgs: []
+      },
+      {
+        project: "Calculadora de Emissões GSS + Inter",
+        description: "Aplicação desenvolvida para o banco Inter que era acessada diretamente do aplicativo do mesmo e permitia a calcular as emissões pessoais e compensar comprando créditos de carbono. A aplicação conta com sistema de login de usuário, sistema de pagamentos, dashboard de usuário e de admin",
+        link: "https://www.gsscarbon.com/calculadora/banco-inter",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "NodeJS", "Styled Components", "REST API", "MongoDB"],
+        imgs: []
+      },
+      {
+        project: "Calculadora de Emissões de Automóveis GP Auto - uma parceria entre GP São Paulo + Smartie Carbon",
+        description: "Aplicação que serviu para que espectadores do GP São Paulo pudessem medir e compensar as emissões de carbono de seus carros. O sistema contou com sistema de login e geração dos certificados em PDF e envio do mesmo para o email do usuário.",
+        link: "https://www.smartiecarbon.com.br/gpsaopaulo",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "NodeJS", "Styled Components", "REST API", "MongoDB"],
+        imgs: []
+      },
+      {
+        project: "Site da Orma Carbon",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "Styled Components"],
+        imgs: []
+      },
+      {
+        project: "Site da GSS",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "Styled Components"],
+        imgs: []
+      },
+      {
+        project: "Site da VBIO",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: ["HTML5", "CSS3", "Javascript", "NextJS", "ReactJS", "NodeJS", "Styled Components", "REST API", "MongoDB"],
+        imgs: []
+      },
+      
+    ]
+  }
 
-    }
-    
-}
+  // {
+  //   project: "",
+  //   description: "",
+  //   link: "",
+  //   stacks: [],
+  //   imgs: []
+  // },
--- pages/print/pt-BR/index.jsx
+++ pages/print/pt-BR/index.jsx
@@ -1,24 +1,150 @@
 import React from "react";
 import styled from "styled-components";
-import Head from 'next/head'
+import Head from "next/head";
+
+import { ptBR as data } from "/pages/components/data.js";
 
 const Index = () => {
+
+  const makePoints = (max, skill) => {
+    let string = ""
+    
+    for (let i = 0; i < max; i++) {
+      if (i < skill) {
+        string = string + "●"
+      } else {
+        string = string + "○"
+      }
+    }
+  
+    return string
+  };
+
   return (
     <>
-    <Head>
-        <title>Curriculo pt-BR</title>
-    </Head>
-    <Page>
-      <Content>
-        <Up>
-            Kevin Pereira Marangoni
-        </Up>
-        <Down>
-            <Left>coisas</Left>
-            <Right>mais coisas</Right>
-        </Down>
-      </Content>
-    </Page>
+      <Head>
+        <title>Curriculo {data.personal.name}</title>
+      </Head>
+      <Page>
+        <Content>
+          <Up>
+            <h1>{data.personal.name}</h1>
+            <h2>
+              {data.personal.position} nível {data.personal.level}
+            </h2>
+            <h3> Pretensão Salárial:{data.personal.salary}</h3>
+          </Up>
+          <Down>
+            <Left>
+              <Links>
+                <div>
+                  <h2>Github</h2>
+                  <a href={data.personal.links.github}>{data.personal.links.github}</a>
+                </div>
+                <div>
+                  <h2>Linkedin</h2>
+                  <a href={data.personal.links.linkedin}>{data.personal.links.linkedin}</a>
+                </div>
+                <div>
+                  <h2>Lattes</h2>
+                  <a href={data.personal.links.lattes}>{data.personal.links.lattes}</a>
+                </div>
+                <div>
+                  <h2>Instagram</h2>
+                  <a href={data.personal.links.instagram}>{data.personal.links.instagram}</a>
+                </div>
+              </Links>
+              <Info>
+                <div>
+                  <h2>Sobre mim</h2>
+                  <p>{data.personal.info.about}</p>
+                </div>
+                <div>
+                  <h2>Procuro por</h2>
+                  <p>{data.personal.info.looking}</p>
+                </div>
+                <div>
+                  <h2>Objetivos</h2>
+                  <p>{data.personal.info.want}</p>
+                </div>
+              </Info>
+              <Soft>
+                    <h1>Softskills</h1>
+                    {data.softskills.length > 0 ? (
+                      data.softskills.map((item, index) => {
+                        return (
+                          <div key={index}>
+                            <h2>
+                              {item.name}
+                            </h2>
+                          </div>
+                        );
+                      })
+                    ) : (
+                      <p></p>
+                    )}
+                  </Soft>
+            </Left>
+            <Right>
+              <div>
+                <h1>Educação</h1>
+                {data.education.length > 0 ? (
+                  data.education.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <h2>{item.instName}</h2>
+                        <p>{item.type}</p>
+                        <p>{item.title}</p>
+                        <p>{item.outDate}</p>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p>Sem educação</p>
+                )}
+              </div>
+              <div>
+                <h1>Experiência Profissional</h1>
+                {data.work.length > 0 ? (
+                  data.work.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <h2>
+                          {item.companyName} - {item.description}
+                        </h2>
+                        <p>{item.howLong}</p>
+                        <p>{item.position}</p>
+                        <p>{item.act}</p>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p>Em busca da primeira oportunidade</p>
+                )}
+              </div>
+              <Skills>
+                  <Hard>
+                    <h1>Stacks</h1>
+                    {data.stacks.length > 0 ? (
+                      data.stacks.map((item, index) => {
+                        return (
+                          <div>
+                            <h2>
+                              {item.name}
+                            </h2>
+                            <p>{ makePoints(5, item.skill) }</p>
+                          </div>
+                        );
+                      })
+                    ) : (
+                      <p></p>
+                    )}
+                  </Hard>
+              </Skills>
+            </Right>
+          </Down>
+        </Content>
+      </Page>
     </>
   );
 };
@@ -26,39 +152,89 @@ const Index = () => {
 export default Index;
 
 const Page = styled.div`
-  *{
+  * {
     color: #000;
     font-family: Arial, sans-serif;
+    font-size: 13px;
   }
-    height: 100vh;
-    margin: 20px;
-    width: 100%;
+  min-height: 100vh;
+  margin: 20px;
+  width: 100%;
   max-width: 210mm;
   margin: 0 auto;
   background-color: #fff;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
 `;
 
 const Content = styled.div`
   padding: 20px;
-  *{
+  * {
     border: 1px dashed red;
   }
-
 `;
 
 const Up = styled.div`
 
-`
+h1{
+  font-size: 40px;
+}
+h2{
+  font-size: 25px;
+}
+h3{
+  font-size: 18px;
+}
+`;
 
 const Down = styled.div`
   display: flex;
-`
+`;
 
 const Left = styled.div`
-    border-right: 1px solid #000;
-    width: 400px;
+  border-right: 1px solid #000;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 const Right = styled.div`
+  
+  h1 {
+    font-size: 18px;
+  }
+`;
+
+const Links = styled.div``;
+
+const Info = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`;
+
+const Skills = styled.div`
+  display: flex;
+`;
+
+const Hard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  height: fit-content;
+  div{
+    width: 23%;
+  }
+  `;
+
+const Soft = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  height: fit-content;
+  div{
+    width: 45%;
+    height: 40px;
+  }
 `;
