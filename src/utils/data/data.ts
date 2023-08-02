3d1428c19b707601a286a64fdc7c9591573c6d20
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-08-02 14:34:22 -0300

    adds images

---
+++ src/utils/data/data.ts
@@ -0,0 +1,264 @@
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import lattes from "/pages/components/assets/img/t/Lattes.png";
+
+import html from "/pages/components/assets/img/t/html.png";
+import css from "/pages/components/assets/img/t/css.png";
+import js from "/pages/components/assets/img/t/js.png";
+import next from "/pages/components/assets/img/t/next2.png";
+import react from "/pages/components/assets/img/t/React2.png";
+import node from "/pages/components/assets/img/t/node.png";
+import ts from "/pages/components/assets/img/t/ts.png";
+import sc from "/pages/components/assets/img/t/styled.png";
+import docker from "/pages/components/assets/img/t/docker.png";
+import actions from "/pages/components/assets/img/t/actions.png";
+import motion from "/pages/components/assets/img/t/framer-motion.png";
+import mui from "/pages/components/assets/img/t/mui.png";
+import rest from "/pages/components/assets/img/t/rest.png";
+import velo from "/pages/components/assets/img/t/velo.png";
+import mongodb from "/pages/components/assets/img/t/mongodb.svg";
+
+import ormaLogo from "/pages/components/assets/img/t/orma logo.svg";
+import gssLogo from "/pages/components/assets/img/t/gss.svg";
+
+import etec from "/pages/components/assets/img/t/etec.png";
+import unimonte from "/pages/components/assets/img/t/unimonte.png";
+import unifesp from "/pages/components/assets/img/t/unifesp.png";
+
+
+export const ptBR = {
+    personal: {
+      name: "Kevin Pereira Marangoni",
+      position: "Desenvolvedor Javascript Web Fullstack",
+      level: "Pleno",
+      salary: "R$ 4500 a R$ 6500",
+      links: {
+        github: "https://github.com/kevinmarangoni",
+        linkedin: "https://www.linkedin.com/in/kevin-marangoni-1a3a17127/",
+        lattes: "http://lattes.cnpq.br/5620060448568039",
+        instagram: "https://www.instagram.com/kevinmarangoni/",
+      },
+      language: [
+        { name: "Português", skill: 5 },
+        { name: "Inglês", skill: 4 },
+        { name: "Espanhol", skill: 2 },
+      ],
+      contact: {
+        phone: "+55 13 98135-7393",
+        whatsapp: "+55 13 98135-7393",
+        email: "kevin-marangoni_@hotmail.com",
+      },
+      info: {
+        about:
+        "Sou um profissional realizado, com Bacharelado em Biomedicina e Mestrado em Ciências da Saúde. Minha trajetória sempre foi impulsionada por uma profunda fascinação pela biologia, combinada com uma genuína paixão por computadores e programação. Ao longo de minhas experiências acadêmicas e profissionais, aprimorei minhas habilidades em ambos os campos, tornando-me um recurso versátil e valioso para qualquer organização.",
+        looking:
+        "Estou ativamente buscando oportunidades para contribuir com minha expertise em projetos e empresas que compartilham do meu compromisso com o crescimento da sociedade e a valorização da diversidade. Acredito firmemente em tratar as pessoas como indivíduos únicos, acolhendo a diversidade e promovendo um ambiente de trabalho inclusivo. Se me for concedida a oportunidade, almejo fazer parte de uma equipe que valorize as perspectivas de cada membro, impulsionando a inovação e a mudança positiva.",
+        want: "Com um amplo conhecimento em ferramentas Fullstack, frameworks e diversas outras tecnologias, estou ansioso para empregar minhas habilidades para atender às necessidades da sua empresa. Tenho o desejo de colaborar com equipes multidisciplinares, reconhecendo o imenso valor em aprender com a diversidade de experiências e conhecimentos de cada colega. Trabalhando juntos, podemos alcançar resultados notáveis e evoluir constantemente neste cenário digital em constante transformação.",
+      },
+    },
+    work: [
+      {
+        logo: ormaLogo,
+        companyName: "Orma Carbon",
+        howLong: "de fev. 2022 a out. 2022",
+        description: "Soluções Tecnológicas para Sustentabilidade",
+        position: "Desenvolvedor Web Frontend Jr.",
+        act: "Desenvolve aplicações ReactJs/NextJs para a empresa",
+        website: "https://www.orma.eco",
+        isCurrent: false,
+      },
+      {
+        logo: gssLogo,
+        companyName: "GSS Carbono e Sustentabilidade",
+        howLong: "de out. 2022 a abr. 2023",
+        description: "Soluções Tecnológicas para Sustentabilidade",
+        position: "Desenvolvedor Web Fullstack Jr.",
+        act: "Desenvolve aplicações ReactJs/NextJs e Node.Js para a empresa",
+        website: "https://www.gss.eco",
+        isCurrent: false,
+      },
+    ],
+    education: [
+      {
+        instName: "Etec Dra. Ruth Cardoso",
+        inDate: "01/2011",
+        outDate: "12/2013",
+        type: "Ensino Médio",
+        title: "Concluído",
+        logo: etec,
+      },
+      {
+        instName: "Etec Dra. Ruth Cardoso",
+        inDate: "06/2012",
+        outDate: "12/2013",
+        type: "Ensino Técnico",
+        title: "Técnico em Informática",
+        logo: etec,
+      },
+      {
+        instName: "Universidade São Judas",
+        inDate: "01/2015",
+        outDate: "12/2018",
+        type: "Faculdade de Biomedicina",
+        title: "Bacharelado em Ciências Biomédicas",
+        logo: unimonte,
+      },
+      {
+        instName: "Universidade Federal de São Paulo",
+        inDate: "01/2019",
+        outDate: "09/2021",
+        type: "Pós-Graduação",
+        title: "Mestrado em Ciências da Saúde",
+        logo: unifesp,
+      },
+    ],
+    stacks: [
+      { logo: html, name: "HTML5", skill: 4 },
+      { logo: css, name: "CSS3", skill: 3 },
+      { logo: js, name: "Javascript", skill: 4 },
+      { logo: ts, name: "Typescript", skill: 3 },
+      { logo: next, name: "NextJS", skill: 4 },
+      { logo: react, name: "ReactJS", skill: 4 },
+      { logo: node, name: "NodeJS", skill: 3 },
+      { logo: mongodb, name: "MongoDB", skill: 3 },
+      { logo: sc, name: "Styled Components", skill: 3 },
+      { logo: docker, name: "Docker", skill: 3 },
+      { logo: actions, name: "Actions", skill: 3 },
+      { logo: motion, name: "Framer Motion", skill: 3 },
+      { logo: mui, name: "Material UI", skill: 3 },
+      { logo: rest, name: "REST API", skill: 4 },
+      { logo: velo, name: "Wix Velo", skill: 3 },
+      { logo: "", name: "MySQL", skill: 3 },
+    ],
+    softskills: [
+      { name: "Empatia", skill: "" },
+      { name: "Comunicação efetiva", skill: "" },
+      { name: "Colaboração", skill: "" },
+      { name: "Pensamento crítico", skill: "" },
+      { name: "Resolução de problemas", skill: "" },
+      { name: "Adaptabilidade", skill: "" },
+      { name: "Autodisciplina", skill: "" },
+    ],
+    portfolio: [
+      {
+        project: "Movimento Repenso",
+        description:
+          "Uma plataforma que permite que pequenas empresas realizem seu inventário anual de emissões de carbono com base em metodologias e diretrizes internacionalmente reconhecidas, como o Protocolo de Gases de Efeito Estufa. A plataforma pode fornecer um questionário estruturado para coletar informações sobre as atividades da empresa, como consumo de energia, transporte, resíduos e outras fontes relevantes. A plataforma conta com sistema de login, sistema de pagamentos e dashboard de usuários e admin, assim como CMS para gerenciamento de conteúdos do blog.",
+        link: "https://www.repenso.eco",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "GoLang",
+          "Styled Components",
+          "REST API",
+          "MySQL",
+        ],
+        imgs: [],
+      },
+      {
+        project: "Smartie Carbon",
+        description:
+          "Plataforma construida pela Orma Carbon para Solví a fim de promover a venda e compra de créditos de carbono, possui uma calculadora de emissões de automóveis e outra para emissões do dia-a-dia. A plataforma conta com sistema de login",
+        link: "https://www.smartiecarbon.com.br",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "NodeJS",
+          "Styled Components",
+          "REST API",
+          "MySQL",
+        ],
+        imgs: [],
+      },
+      {
+        project: "Calculadora de Emissões GSS + Inter",
+        description:
+          "Aplicação desenvolvida para o banco Inter que era acessada diretamente do aplicativo do mesmo e permitia a calcular as emissões pessoais e compensar comprando créditos de carbono. A aplicação conta com sistema de login de usuário, sistema de pagamentos, dashboard de usuário e de admin",
+        link: "https://www.gsscarbon.com/calculadora/banco-inter",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "NodeJS",
+          "Styled Components",
+          "REST API",
+          "MongoDB",
+        ],
+        imgs: [],
+      },
+      {
+        project:
+          "Calculadora de Emissões de Automóveis GP Auto - uma parceria entre GP São Paulo + Smartie Carbon",
+        description:
+          "Aplicação que serviu para que espectadores do GP São Paulo pudessem medir e compensar as emissões de carbono de seus carros. O sistema contou com sistema de login e geração dos certificados em PDF e envio do mesmo para o email do usuário.",
+        link: "https://www.smartiecarbon.com.br/gpsaopaulo",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "NodeJS",
+          "Styled Components",
+          "REST API",
+          "MongoDB",
+        ],
+        imgs: [],
+      },
+      {
+        project: "Site da Orma Carbon",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "Styled Components",
+        ],
+        imgs: [],
+      },
+      {
+        project: "Site da GSS",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "Styled Components",
+        ],
+        imgs: [],
+      },
+      {
+        project: "Site da VBIO",
+        description: "Manutenção e atualização do site intitucional da empresa",
+        link: "",
+        stacks: [
+          "HTML5",
+          "CSS3",
+          "Javascript",
+          "NextJS",
+          "ReactJS",
+          "NodeJS",
+          "Styled Components",
+          "REST API",
+          "MongoDB",
+        ],
+        imgs: [],
+      },
+    ],
+  };
+  
